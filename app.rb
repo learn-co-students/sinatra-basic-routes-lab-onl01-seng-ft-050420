7ec51b0879fdc09c9296275a2f187a60f90d87b9
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-10 10:25:38 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello World"
+    end
+    
+    get '/name' do
+        "My name is Kelsey"
+    end
+
+    get '/hometown' do
+        "My hometown is Cheyenne, WY"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Sixteeen Tons"
+    end
+
 end
