a4ae447a6c578d946cb3ecd6fd7841988f0a3613
anette87 <anettem.rivera@gmail.com>
2020-06-11 16:11:20 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Anette"
+    end 
+
+    get '/hometown' do 
+        "My hometown is San Juan, PR"
+    end 
+
+    get '/favorite-song' do
+        "My favorite song is Drop of Jupiter"
+    end 
 end
