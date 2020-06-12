78cf129c0f5c0473f0c3b2b80cfcd618e5b52e63
Adamlatorraca <adam.latorraca@gmail.com>
2020-06-12 17:12:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World!"
+      end
+    
+    get '/name' do
+       "My name is Adam"
+    end
+    
+    get '/hometown' do
+        "My hometown is New Rochelle"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Teach Me How To Dougie"
+    end
 end
