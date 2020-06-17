a5f9396c911ae991783f1a602a5bf1eca8a1a202
emerykurt <emerylumsden@gmail.com>
2020-06-17 18:27:28 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Emery"
+    end
+
+    get '/hometown' do
+        "My hometown is Miami."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'Love is You' by Chrisette Michele"
+    end
 end
